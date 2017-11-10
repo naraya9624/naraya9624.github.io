ba21c98dc1ba0b0e073e38a465cd9d1718af73b7
naraya9624 <jungkeuny@naver.com>
2017-11-10 20:29:42 +0900

    upload all folders

---
+++ dm/files/01__당뇨환자_서비스용_스토리보드_변경이력_/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,I,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="01__당뇨환자_서비스용_스토리보드_변경이력_.html",d="generationDate",e=new Date(1510309923333.16),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="99f90566e3ae4ef69ce0c37ec8aef012",q="type",r="Axure:Page",s="name",t="01. 당뇨환자 서비스용(스토리보드 변경이력)",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="near",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
