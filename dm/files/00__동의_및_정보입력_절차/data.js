283ddaa4b84e0fb494d76a4533e3eed91e430d72
naraya9624 <jungkeuny@naver.com>
2017-11-20 13:52:15 +0900

    dm deleted

--- dm/files/00__동의_및_정보입력_절차/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,I,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
-var b="url",c="00__동의_및_정보입력_절차.html",d="generationDate",e=new Date(1511153056673.4),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="6edfa5413d234ffdbb031ccb78f91d55",q="type",r="Axure:Page",s="name",t="00. 동의 및 정보입력 절차",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="near",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
-return _creator();
-})());
